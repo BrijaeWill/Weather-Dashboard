5bfc31e557d487052b4227a509da637bc40d691a
Brijae <brijaewilliams5@gmail.com>
2024-11-21 17:31:14 -0500

    Added server code

--- server/src/server.ts
+++ server/src/server.ts
@@ -10,13 +10,17 @@ import routes from './routes/index.js';
 const app = express();
 
 const PORT = process.env.PORT || 3001;
+// TODO: Implement middleware for parsing JSON and urlencoded form data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // TODO: Serve static files of entire client dist folder
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+app.use(express.static(path.join(__dirname, '../../client/dist')));
 
 
-// TODO: Implement middleware for parsing JSON and urlencoded form data
 
-// TODO: Implement middleware to connect the routes
 app.use(routes);
 
 // Start the server on the port
